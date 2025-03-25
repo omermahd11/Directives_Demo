37bac5170f9039c933a36f27ac0cd40bcd073262
Omer <omer.mohamed@sheridancollege.ca>
2025-03-25 09:39:33 -0400

    @For

--- DirectiveExample1/src/app/app.component.ts
+++ DirectiveExample1/src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DirectiveExample1';
+  title = 'Directives Example 1';
+ 
+  newItem = ""
+
+  items = [" Item 1", "Item 2" , "Item 3", "Item 4"]
+
+  addItem(){
+
+    this.items.push(this.newItem)
+  }
+
+  
 }
