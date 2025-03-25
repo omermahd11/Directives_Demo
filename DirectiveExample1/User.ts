36f2db98b2e5f0463acc0f33fe7cf4520c4708c9
Omer <omer.mohamed@sheridancollege.ca>
2025-03-25 10:48:45 -0400

    final commit

---
+++ DirectiveExample1/User.ts
@@ -0,0 +1,12 @@
+export class User {
+    id : number
+    name :string 
+    age : number
+
+
+constructor( id: number, name : string , age : number) {
+  this.id = id
+  this.name = name;
+  this.age = age
+}
+}
--- DirectiveExample1/src/app/app.component.ts
+++ DirectiveExample1/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { User } from '../../User';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +15,38 @@ export class AppComponent {
  
   newItem = ""
 
-  items : string[] = []
+  items : string[] = ["Item 1", "Item 2"]
 
   addItem(){
 
     this.items.push(this.newItem)
   }
 
+  getColor(item : string){
+
+    if(item == "Item 1" || item == "item 2"){
+
+      return "red"
+    } else {
+
+      return 'blue'
+    }
+  }
   
+
+  names = ["Tim" , "Jane" , "Reem" , "Mark"]
+
+
+  applyClass( name :string): boolean {
+
+    if (name == "Tim"){
+      return true; 
+    }else{
+      return false;
+    }
+  }
+
+
+  users :User[] = [ new User(100, "Jane" , 25) ,  new User(200, "Reem" , 20),  new User(300, "Tim" , 22)]
 }
+
