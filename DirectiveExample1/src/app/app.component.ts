5a36e2fe053a0430537236486a7d89eab08d5ba6
Omer <omer.mohamed@sheridancollege.ca>
2025-03-25 09:59:44 -0400

    *ngFor example is added

--- DirectiveExample1/src/app/app.component.ts
+++ DirectiveExample1/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , FormsModule],
+  imports: [RouterOutlet , FormsModule , CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
